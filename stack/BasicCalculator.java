541ddce316ed24a6420060d95e1f3633c25d80dd
Chen Li <cherishchen8@gmail.com>
2019-09-18 23:01:51 -0700

    stack 1st commit

---
+++ stack/BasicCalculator.java
@@ -0,0 +1,41 @@
+package stack;
+
+import java.util.Stack;
+
+public class BasicCalculator {
+
+	public int solution(String s) {
+		Stack<Integer> stack = new Stack<>();
+		int result = 0, sign = 1, num = 0;
+		char[] ca = s.toCharArray();
+
+		for (int i = 0; i < ca.length; i++) {
+			char c = ca[i];
+			if (Character.isDigit(c)) {
+				num = 10 * num + (int) (c - '0');
+			} else if (c == '+') {
+				result += sign * num;
+				num = 0;
+				sign = 1;
+			} else if (c == '-') {
+				result += sign * num;
+				num = 0;
+				sign = -1;
+			} else if (c == '(') {
+				stack.push(result);
+				stack.push(sign);
+				sign = 1;
+				result = 0;
+			} else if (c == ')') {
+				result += sign * num;
+				num = 0;
+				result *= stack.pop();
+				result += stack.pop();
+			}
+		}
+		if (num != 0)
+			result += sign * num;
+		return result;
+	}
+
+}
---
+++ stack/DailyTemperatures.java
@@ -0,0 +1,21 @@
+package stack;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+	public int[] solution(int[] T) {
+		int[] result = new int[T.length];
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = 0; i < T.length; i++) {
+			while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+				int j = stack.pop();
+				result[j] = i - j;
+			}
+			stack.push(i);
+		}
+		return result;
+	}
+
+}
